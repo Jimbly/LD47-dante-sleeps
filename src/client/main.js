9e624173abf796924c902623b8b1b64b646fa957
Jimb Esser <wasteland@gmail.com>
2020-10-02 20:35:23 -0700

    Refactor motion logic to be based around radius; Add some art

--- src/client/main.js
+++ src/client/main.js
@@ -6,8 +6,9 @@ const camera2d = require('./glov/camera2d.js');
 const engine = require('./glov/engine.js');
 // const glov_font = require('./glov/font.js');
 const input = require('./glov/input.js');
-const { cos, max, min, sin, PI } = Math;
+const { cos, max, min, sin, tan, PI } = Math;
 const net = require('./glov/net.js');
+const pico8 = require('./glov/pico8.js');
 const { randCreate } = require('./glov/rand_alea.js');
 const glov_sprites = require('./glov/sprites.js');
 // const sprite_animation = require('./glov/sprite_animation.js');
@@ -78,8 +79,13 @@ export function main() {
   const PAD = input.PAD;
 
   function initGraphics() {
-    sprites.test = createSprite({
-      name: 'test',
+    sprites.rock = createSprite({
+      name: 'rock',
+      size: vec2(16, 16),
+      origin: vec2(0.5,0.5),
+    });
+    sprites.player = createSprite({
+      name: 'plane',
       size: vec2(16, 16),
       origin: vec2(0.5,0.5),
     });
@@ -91,9 +97,11 @@ export function main() {
 
   let state = {
     cam_x: 0,
-    pos: vec2(150, 200),
-    angle: 0,
-    speed: 1,
+    player: {
+      pos: vec2(150, 200),
+      angle: PI,
+      radius: 1,
+    },
     rocks: [],
   };
   let rand = randCreate(1);
@@ -101,78 +109,136 @@ export function main() {
     state.rocks.push({
       pos: vec2(ii * 16 + rand.random() * 16, rand.floatBetween(16, game_height - 16)),
       angle: rand.floatBetween(0, PI * 2),
+      rspeed: rand.floatBetween(-1, 1),
       color: vec4(0.1, 0.1, 0.1, 1),
     });
   }
 
+
+  const base_radius = 50;
   const speed_scale = 0.75;
   const dTheta = 0.002 * speed_scale;
-  const base_speed = 0.1 * speed_scale;
   const accel = 0.0025;
   function test(dt) {
     sprites.game_bg.draw({
       x: 0, y: 0, z: Z.BACKGROUND,
-      color: [0, 0.72, 1, 1]
+      color: pico8.colors[1],
     });
-    camera2d.set(state.cam_x, 0, state.cam_x + game_width, game_height);
-    let { speed } = state;
+    camera2d.setAspectFixed(game_width, game_height);
+    camera2d.set(camera2d.x0() + state.cam_x, camera2d.y0(), camera2d.x1() + state.cam_x, camera2d.y1());
+    let { player } = state;
+    let { radius } = player;
     if (input.keyDown(KEYS.D)) {
-      state.speed = min(2, speed + dt * accel);
+      player.radius = min(2, radius + dt * accel);
     } else if (input.keyDown(KEYS.A)) {
-      state.speed = max(0.5, speed - dt * accel);
-    } else if (speed > 1) {
-      state.speed = max(1, speed - dt * accel * 2);
-    } else if (speed < 1) {
-      state.speed = min(1, speed + dt * accel * 2);
+      player.radius = max(0.5, radius - dt * accel);
+    } else if (radius > 1) {
+      player.radius = max(1, radius - dt * accel * 2);
+    } else if (radius < 1) {
+      player.radius = min(1, radius + dt * accel * 2);
     }
-    // Instead of speed, just use a radius?
     // Instead of doing this, just reduce thrust if we'd go outside?
-    let last_pos = state.pos.slice(0);
-    let test_pos = state.pos.slice(0);
-    let test_dt = 100;
-    let test_dp = test_dt * base_speed;
-    let test_angle = state.angle - test_dt * 0.5 * dTheta;
-    let offs = 0;
-    let oob = false;
-    for (let ii = 0; ii < 30; ++ii) {
-      test_pos[0] += state.speed * test_dp * cos(test_angle);
-      test_pos[1] += state.speed * test_dp * sin(test_angle);
-      test_angle -= test_dt * dTheta;
-      // ui.drawLine(last_pos[0], last_pos[1], test_pos[0], test_pos[1], Z.SPRITES - 1, 2, 1, [0,0,0,0.5]);
-      v2copy(last_pos, test_pos);
-      if (test_pos[1] < 0) {
-        offs = max(offs, -test_pos[1]);
-        oob = true;
-      } else if (test_pos[1] > game_height) {
-        offs = min(offs, game_height - test_pos[1]);
-      }
-    }
+    // let last_pos = state.pos.slice(0);
+    // let test_pos = state.pos.slice(0);
+    // let test_dt = 100;
+    // let test_dp = test_dt * base_speed;
+    // let test_angle = state.angle - test_dt * 0.5 * dTheta;
+    // let offs = 0;
+    // let oob = false;
+    // for (let ii = 0; ii < 30; ++ii) {
+    //   test_pos[0] += state.speed * test_dp * cos(test_angle);
+    //   test_pos[1] += state.speed * test_dp * sin(test_angle);
+    //   test_angle -= test_dt * dTheta;
+    //   // ui.drawLine(last_pos[0], last_pos[1], test_pos[0], test_pos[1], Z.SPRITES - 1, 2, 1, [0,0,0,0.5]);
+    //   v2copy(last_pos, test_pos);
+    //   if (test_pos[1] < 0) {
+    //     offs = max(offs, -test_pos[1]);
+    //     oob = true;
+    //   } else if (test_pos[1] > game_height) {
+    //     offs = min(offs, game_height - test_pos[1]);
+    //   }
+    // }
     // if (oob) {
     //   state.speed = max(0.5, speed - dt * 0.005);
     // }
-    let dx = cos(state.angle);
-    let dy = sin(state.angle);
-    let dp = state.speed * dt * base_speed;
-    state.pos[0] += dp * dx;
-    state.pos[1] += dp * dy;
-    state.cam_x = min(max(state.cam_x, state.pos[0] - game_width * 2 / 3), state.pos[0] - game_width / 3);
-    state.angle -= dt * dTheta;
-    // state.pos[1] += offs;
-    sprites.test.draw({
-      x: state.pos[0],
-      y: state.pos[1],
+    radius = player.radius * base_radius;
+    let { angle } = player;
+    // Test angle against the top of the screen (y = 0)
+    let test_angle = angle;
+    let rbias = 0;
+    if (test_angle > PI) {
+      // coming down from the top, scale back the max radius limit
+      test_angle = PI * 2 - test_angle;
+      if (test_angle < PI / 2) {
+        rbias = test_angle * base_radius * 2;
+      } else {
+        rbias = (PI - test_angle) * base_radius * 2;
+      }
+    }
+    let dist_to_top = player.pos[1];
+    let inter_angle = (PI - test_angle) / 2;
+    let hoffs = tan(inter_angle) * dist_to_top;
+    let max_r = hoffs / sin(PI - test_angle) - 0.5 + rbias;
+    //let maxs_center = vec2(player.pos[0] + max_r * cos(angle + PI/2), player.pos[1] + max_r * sin(angle + PI/2));
+    //ui.drawHollowCircle(maxs_center[0], maxs_center[1], Z.SPRITES - 1, max_r, 0.99, [1,1,1, 0.5]);
+
+    // Test angle against the bottom of the screen (y = 0)
+    test_angle = angle - PI;
+    if (test_angle < 0) {
+      test_angle += PI * 2;
+    }
+    rbias = 0;
+    if (test_angle > PI) {
+      test_angle = PI * 2 - test_angle;
+      if (test_angle < PI / 2) {
+        rbias = test_angle * base_radius * 2;
+      } else {
+        rbias = (PI - test_angle) * base_radius * 2;
+      }
+    }
+    let dist_to_bottom = game_height - player.pos[1];
+    inter_angle = (PI - test_angle) / 2;
+    hoffs = tan(inter_angle) * dist_to_bottom;
+    let max_r2 = hoffs / sin(PI - test_angle) - 0.5 + rbias;
+    // let maxs_center = vec2(player.pos[0] + max_r2 * cos(angle + PI/2), player.pos[1] + max_r2 * sin(angle + PI/2));
+    // ui.drawHollowCircle(maxs_center[0], maxs_center[1], Z.SPRITES - 1, max_r2, 0.99, [1,1,1, 0.5]);
+
+    // ui.print(null, 50, 50, Z.SPRITES + 10, `angle: ${(angle * 180 / PI).toFixed(0)}`);
+    if (isFinite(max_r)) {
+      radius = min(radius, min(max_r, max_r2));
+    }
+    // let dir = vec2(cos(angle), sin(angle));
+    let new_angle = angle - dt * dTheta;
+    if (new_angle < 0) {
+      new_angle += PI * 2;
+    }
+    let center = vec2(player.pos[0] + radius * cos(angle + PI/2), player.pos[1] + radius * sin(angle + PI/2));
+    let new_pos = vec2(center[0] - radius * cos(new_angle + PI/2), center[1] - radius * sin(new_angle + PI/2));
+    player.pos[0] = new_pos[0];
+    player.pos[1] = new_pos[1];
+    state.cam_x = min(max(state.cam_x, player.pos[0] - game_width * 2 / 3), player.pos[0] - game_width / 3);
+    player.angle = new_angle;
+    sprites.player.draw({
+      x: player.pos[0],
+      y: player.pos[1],
       z: Z.SPRITES,
-      rot: state.angle + PI/2 + 0.3,
+      rot: player.angle + PI,
       color: [1, 1, 1, 1],
     });
     for (let ii = 0; ii < state.rocks.length; ++ii) {
       let r = state.rocks[ii];
-      let hit = v2distSq(r.pos, state.pos) < 8*8;
+      r.angle += r.rspeed * dt * 0.0002;
+      let hit = v2distSq(r.pos, player.pos) < 8*8;
       if (hit) {
+        if (!r.hit) {
+          r.hit = true;
+          state.player.radius = 0.5;
+          state.player.angle += rand.floatBetween(0.5, 0.75);
+        }
         r.color[0] = 1;
         r.color[3] = 1;
       }
-      sprites.test.draw({
+      sprites.rock.draw({
         x: r.pos[0],
         y: r.pos[1],
         z: Z.SPRITES,
